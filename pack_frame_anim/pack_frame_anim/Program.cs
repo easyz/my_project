9314d86aa17b52dc4d1b21ec64790bc676f090dd
yiz <yinzhi523@163.com>
2017-11-14 21:24:55 +0800

    update

--- pack_frame_anim/pack_frame_anim/Program.cs
+++ pack_frame_anim/pack_frame_anim/Program.cs
@@ -72,8 +72,14 @@ namespace merger_eff_tex_lib {
             }
 
             string[] allDir = Directory.GetDirectories(baseDir + exportData["path"] + "\\", "*", SearchOption.TopDirectoryOnly);
-            foreach (string s in allDir) {
-                SingleFile(s + "\\", outBaseDir + exportData["outPath"] + "\\", Path.GetFileNameWithoutExtension(s), exportData["action"]);
+            if (exportData["action"].Keys.Contains("")) {
+                foreach (string s in allDir) {
+                    SingleFile2(s + "\\", outBaseDir + exportData["outPath"] + "\\", Path.GetFileNameWithoutExtension(s), exportData["action"][""]);
+                }
+            } else {
+                foreach (string s in allDir) {
+                    SingleFile(s + "\\", outBaseDir + exportData["outPath"] + "\\", Path.GetFileNameWithoutExtension(s), exportData["action"]);
+                }
             }
 
             Console.WriteLine("Finish!!!");
@@ -114,6 +120,36 @@ namespace merger_eff_tex_lib {
 
             foreach (KeyValuePair<string, List<string>> pair in dict) {
                 //                Console.WriteLine(pair.Key + "  " + string.Join(", ", pair.Value.ToArray()));
+                Handle(pair.Value.ToArray(), outDir, pair.Key, frameDict[pair.Key]);
+            }
+
+            Console.WriteLine(">> 输出 " + name);
+        }
+
+        static void SingleFile2(string dir, string outDir, string name, JsonData actData) {
+            
+            HashSet<string> allFiels = new HashSet<string>();
+            foreach (string VARIABLE in Directory.GetFiles(dir, "*", SearchOption.TopDirectoryOnly)) {
+                allFiels.Add(Path.GetFileNameWithoutExtension(VARIABLE));
+            }
+
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+            Dictionary<string, int> frameDict = new Dictionary<string, int>();
+
+            List<string> list = new List<string>();
+            for (int i = (int) actData[1]; i <= (int) actData[2]; i++) {
+                string fileName = "0" + i.ToString("D4");
+                if (!allFiels.Contains(fileName)) {
+                    Logger.LogError("不存在文件 => " + fileName + " 忽略待机类型 ");
+                    return;
+                }
+                list.Add(dir + fileName + ".png");
+            }
+            string dictName = name;
+            dict.Add(dictName, list);
+            frameDict.Add(dictName, (int)actData[0]);
+
+            foreach (KeyValuePair<string, List<string>> pair in dict) {
                 Handle(pair.Value.ToArray(), outDir, pair.Key, frameDict[pair.Key]);
             }
 
